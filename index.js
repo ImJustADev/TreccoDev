5a88f2ceadc9e086861bed9495de9d605fcd1545
BlazeTheSnep <justadevcontact@gmail.com>
2021-02-22 18:13:44 -0500

    fix stuff

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login("ODEyNzIzMDgyMTI0MTk4MDAw.YDE5Wg.pX7c1PFkwv6mS7afCWvQPkNDJhc");
+client.login("ODEyNzIzMDgyMTI0MTk4MDAw.YDE5Wg.i7bJgNO_QuDUEI4otiQJA5QF5Uc");
 
 client.on('ready', () => {
     console.info('Logged in as ${client.user.tag}!');
