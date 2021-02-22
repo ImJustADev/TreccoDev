249aae6809a0519f17b38f5939034985eb172143
BlazeTheSnep <justadevcontact@gmail.com>
2021-02-22 18:11:44 -0500

    Add ping listener to index.js in main

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ const client = new Discord.Client();
 client.login("ODEyNzIzMDgyMTI0MTk4MDAw.YDE5Wg.pX7c1PFkwv6mS7afCWvQPkNDJhc");
 
 client.on('ready', () => {
-    console.info('Logged in as ${bot.user.tag}!');
+    console.info('Logged in as ${client.user.tag}!');
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+        msg.channel.send('pong');
+    }
 });
